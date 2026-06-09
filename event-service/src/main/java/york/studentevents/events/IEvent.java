7de329bd7d420ee51244bc4168bc608bd93f6d9c
Jamie <jamieramsell07@outlook.com>
2026-06-10 00:23:59 +0100

    feat: implement IEvent interface

--- event-service/src/main/java/york/studentevents/events/IEvent.java
+++ event-service/src/main/java/york/studentevents/events/IEvent.java
@@ -1,5 +1,21 @@
 package york.studentevents.events;
 
-public class IEvent {
-    
+import java.time.LocalDateTime;
+
+public interface IEvent {
+
+  public long getId();
+
+  public String getTitle();
+
+  public String getDescription();
+
+  public LocalDateTime getDate();
+
+  public String getLocation(); // todo: implement venues
+
+  public int getCapacity();
+
+  public String getCategory(); // todo: implement categories
+
 }
