78f9cb53f0d9043eb6c3ab42a98a3f9a908f80be
Sruthiksha <sruthikshaa07@gmail.com>
2026-06-10 17:28:59 +0100

    feat : defined IRepository generic interface

--- event-service/src/main/java/york/studentevents/repository/IRepository.java
+++ event-service/src/main/java/york/studentevents/repository/IRepository.java
@@ -1,5 +1,12 @@
 package york.studentevents.repository;
 
-public class IRepository {
-    
+import java.util.Optional;
+import java.util.List;
+
+public interface IRepository<T>{
+    void save (T entity);
+    void delete (Long id);
+    Optional <T> findByID(Long id);
+    List <T> findAll();
+
 }
