a9e507703d7ecb88f1b8ec5158e1785c84781b09
Sruthiksha <sruthikshaa07@gmail.com>
2026-06-11 02:06:05 +0100

    docs: added JavaDoc to IRepository Interface

--- event-service/src/main/java/york/studentevents/repository/IRepository.java
+++ event-service/src/main/java/york/studentevents/repository/IRepository.java
@@ -4,9 +4,29 @@ import java.util.Optional;
 import java.util.List;
 
 public interface IRepository<T>{
+    /**
+     * The selected entity is saved to repo.
+     * @param entity saves a selected entity
+     */
     void save (T entity);
+    
+    /**
+     * Selects an id, removes that entity from the repository - nothing is returned
+     * @param id selected is removed
+     */
     void delete (Long id);
-    Optional <T> findByID(Long id);
+    
+    /**
+     * Finds an entity by its unique identifier
+     * @param id the unique identifier of the entity
+     * @return an Optional containing the entity if found, empty if not
+     */
+    Optional <T> findById(Long id);
+
+    /**
+     * Retrieves all entities from the repository
+     * @return List of all entities, or an empty List if none exist
+     */
     List <T> findAll();
 
 }
