ac3ce889d147613088dbc8835d87e5e308c7eb12
Jamie Ramsell <69736616+jamieramsell@users.noreply.github.com>
2026-06-11 20:04:57 +0100

    feat: overload constructor with a method which doesn't require a message

--- event-service/src/main/java/york/studentevents/exceptions/VenueNotFoundException.java
+++ event-service/src/main/java/york/studentevents/exceptions/VenueNotFoundException.java
@@ -1,7 +1,13 @@
 package york.studentevents.exceptions;
 
 public class VenueNotFoundException extends RuntimeException {
+    
     public VenueNotFoundException(String message) {
         super(message);
     }
+
+    public VenueNotFoundException() {
+        super();
+    }
+    
 }
