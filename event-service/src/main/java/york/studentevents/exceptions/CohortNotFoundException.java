f91c9cef55820be3661c2f675347bebb9f44aa59
Jamie Ramsell <69736616+jamieramsell@users.noreply.github.com>
2026-06-11 20:03:23 +0100

    feat: overload constructor with a method which doesn't require a message

--- event-service/src/main/java/york/studentevents/exceptions/CohortNotFoundException.java
+++ event-service/src/main/java/york/studentevents/exceptions/CohortNotFoundException.java
@@ -1,8 +1,13 @@
 package york.studentevents.exceptions;
 
 public class CohortNotFoundException extends RuntimeException {
+    
     public CohortNotFoundException(String message) {
         super(message);
+    }
+
+    public CohortNotFoundException() {
+        super();
     }
     
 }
