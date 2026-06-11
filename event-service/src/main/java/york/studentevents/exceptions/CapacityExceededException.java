747bf999ecd5c794f2cb13cbdca100818b6cb014
Jamie Ramsell <69736616+jamieramsell@users.noreply.github.com>
2026-06-11 20:02:48 +0100

    feat: overload constructor with a method which doesn't require a message

--- event-service/src/main/java/york/studentevents/exceptions/CapacityExceededException.java
+++ event-service/src/main/java/york/studentevents/exceptions/CapacityExceededException.java
@@ -1,7 +1,13 @@
 package york.studentevents.exceptions;
 
 public class CapacityExceededException extends RuntimeException {
+    
     public CapacityExceededException(String message) {
         super(message);
     }
+
+    public CapacityExceededException() {
+        super();
+    }
+    
 }
