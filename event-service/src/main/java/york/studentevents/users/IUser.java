1f95dac26a09e8241e0fb6de8bcc1490182a2dbe
Sruthiksha <sruthikshaa07@gmail.com>
2026-06-15 20:30:00 +0100

    feat: added indentations

--- event-service/src/main/java/york/studentevents/users/IUser.java
+++ event-service/src/main/java/york/studentevents/users/IUser.java
@@ -5,12 +5,12 @@ public interface IUser{
     /** Returns the unique identifier of the user
      * @return the user's ID
      */
-public long getId();
-public String getUsername();
-public String getEmail();
-public long getCohort();
+    public long getId();
+    public String getUsername();
+    public String getEmail();
+    public long getCohort();
     /** Returns a list of events user signed up to as list of Long eventIds
      * @return List of Long eventIds
      */
-public List<Long> getRegisteredEvents();
+    public List<Long> getRegisteredEvents();
 }
