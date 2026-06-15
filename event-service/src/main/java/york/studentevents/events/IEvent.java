3c1d00b6f064b4642ae5d0bd4297e35a4822f458
Jamie <jamieramsell07@outlook.com>
2026-06-15 18:36:48 +0100

    feat: add setters for all attributes except id

--- event-service/src/main/java/york/studentevents/events/IEvent.java
+++ event-service/src/main/java/york/studentevents/events/IEvent.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 public interface IEvent {
 
+  // Getters //
+
   long getId();
 
   String getTitle();
@@ -19,5 +21,19 @@ public interface IEvent {
   Integer getCapacity();
 
   String getCategory(); // todo: implement categories
+
+  // Setters //
+
+  void setTitle(String title);
+
+  void setDescription(String description);
+
+  void setDateTime(LocalDateTime startDateTime, LocalDateTime endDateTime);
+
+  void setLocation(String location); // todo: implement venues
+
+  void setCapacity(Integer capacity);
+
+  void setCategory(String category); // todo: implement categories
 
 }
