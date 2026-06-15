1cad9e2b2957e17584f93da2cd9dfc56676ee90d
Sruthiksha <sruthikshaa07@gmail.com>
2026-06-15 20:46:05 +0100

    feat: removed public modifiers

--- event-service/src/main/java/york/studentevents/users/IUser.java
+++ event-service/src/main/java/york/studentevents/users/IUser.java
@@ -2,15 +2,20 @@ package york.studentevents.users;
 import java.util.List;
 /** Defines the core contract for user covering their profile data and relationships */
 public interface IUser{
+    
     /** Returns the unique identifier of the user
      * @return the user's ID
      */
-    public long getId();
-    public String getUsername();
-    public String getEmail();
-    public long getCohort();
+    long getId();
+    
+    String getUsername();
+    
+    String getEmail();
+    
+    long getCohort();
+    
     /** Returns a list of events user signed up to as list of Long eventIds
      * @return List of Long eventIds
      */
-    public List<Long> getRegisteredEvents();
+    List<Long> getRegisteredEvents();
 }
