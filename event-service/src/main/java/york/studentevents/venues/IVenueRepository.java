f5d68c9bbada4cb56f571995f50b802924bd0037
Jamie <jamieramsell07@outlook.com>
2026-06-17 02:19:38 +0100

    feat: implement IVenueRepository

--- event-service/src/main/java/york/studentevents/venues/IVenueRepository.java
+++ event-service/src/main/java/york/studentevents/venues/IVenueRepository.java
@@ -1,5 +1,14 @@
 package york.studentevents.venues;
 
-public class IVenueRepository {
-    
-}
+import york.studentevents.repository.IRepository;
+
+/**
+ * Repository for storing and retrieving {@link IVenue} entities.
+ *
+ * <p>Extends {@link york.studentevents.repository.IRepository} with {@link IVenue} as the
+ * managed type, providing standard CRUD operations scoped to vsnuew.
+ *
+ * @see york.studentevents.repository.IRepository
+ * @see IVenue
+ */
+public interface IVenueRepository extends IRepository<IVenue> {}
