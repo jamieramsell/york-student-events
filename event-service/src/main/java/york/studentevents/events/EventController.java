107bc7f5afde908389df2552b71eddac293dd224
Jamie <jamieramsell07@outlook.com>
2026-06-18 21:46:20 +0100

    feat: implement a thin EventController using REST API

--- event-service/src/main/java/york/studentevents/events/EventController.java
+++ event-service/src/main/java/york/studentevents/events/EventController.java
@@ -1,5 +1,22 @@
 package york.studentevents.events;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EventController {
-    
+  
+  private final EventService eventService;
+
+  public EventController(EventService service) {
+    this.eventService = service;
+  }
+
+  @GetMapping("/events") // Map method to HTTP 'GET /events' request
+  public List<IEvent> getAllEvents() {
+    return eventService.getAllEvents();
+  }
+
 }
