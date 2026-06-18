a76d865de6d11ab2fc41785f5ba2442c6f34eb11
Jamie <jamieramsell07@outlook.com>
2026-06-18 16:33:44 +0100

    feat!: implement IEntity into IUser and ICohort, which means migrating from long IDs to UUIDs.

--- event-service/src/main/java/york/studentevents/users/IUser.java
+++ event-service/src/main/java/york/studentevents/users/IUser.java
@@ -3,21 +3,19 @@ package york.studentevents.users;
 import java.util.List;
 import java.util.UUID;
 
+import york.studentevents.repository.IEntity;
+
 /** Defines the core contract for user covering their profile data and relationships */
-public interface IUser{
-    
-    /** Returns the unique identifier of the user
-     * @return the user's ID
-     */
-    long getId();
+public interface IUser extends IEntity{
     
     String getUsername();
     
     String getEmail();
     
-    long getCohort();
+    UUID getCohort();
     
     /** Returns a list of events that the user has signed up to as list of event IDs
+     * 
      * @return List of eventIds
      */
     List<UUID> getRegisteredEvents();
