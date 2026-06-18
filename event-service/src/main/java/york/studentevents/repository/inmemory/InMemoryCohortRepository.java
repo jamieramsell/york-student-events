9205ef7f55911033dfff44c858e0de0e8b34be17
Jamie <jamieramsell07@outlook.com>
2026-06-18 16:36:31 +0100

    feat!: remove existing repository interface stubs; replace them with in-memory implementations of repositories within the repository package

---
+++ event-service/src/main/java/york/studentevents/repository/inmemory/InMemoryCohortRepository.java
@@ -0,0 +1,17 @@
+package york.studentevents.repository.inmemory;
+
+import york.studentevents.cohorts.ICohort;
+
+/**
+ * Hash-map backed repository for storing and retrieving {@link ICohort} entities.
+ *
+ * <p>Extends {@link york.studentevents.repository.AbstractInMemoryRepository} with {@link ICohort}
+ * as the managed type, providing standard CRUD operations scoped to cohorts.
+ *
+ * <p>Used for integration testing before implementing database-backed repositories.
+ * 
+ * @see york.studentevents.repository.IRepository
+ * @see AbstractInMemoryRepository
+ * @see ICohort
+ */
+public class InMemoryCohortRepository extends AbstractInMemoryRepository<ICohort> {}
--- event-service/src/main/java/york/studentevents/repository/inmemory/InMemoryUserRepository.java
+++ event-service/src/main/java/york/studentevents/repository/inmemory/InMemoryUserRepository.java
@@ -1,5 +1,17 @@
 package york.studentevents.repository.inmemory;
 
-public class InMemoryUserRepository {
-    
-}
+import york.studentevents.users.IUser;
+
+/**
+ * Hash-map backed repository for storing and retrieving {@link IUser} entities.
+ *
+ * <p>Extends {@link york.studentevents.repository.AbstractInMemoryRepository} with {@link IUser}
+ * as the managed type, providing standard CRUD operations scoped to users.
+ *
+ * <p>Used for integration testing before implementing database-backed repositories.
+ * 
+ * @see york.studentevents.repository.IRepository
+ * @see AbstractInMemoryRepository
+ * @see IUser
+ */
+public class InMemoryUserRepository extends AbstractInMemoryRepository<IUser> {}
