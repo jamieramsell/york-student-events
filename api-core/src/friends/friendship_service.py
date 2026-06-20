87ab7003bd635099cf856ab5b457306d078b0c81
Jamie <jamieramsell07@outlook.com>
2026-06-20 03:11:58 +0100

    fix!: move some methods to a new base module to avoid circular imports
    * note that this push also removest the Friendship._accept() method, due to a more elegant solution being found, see below:
    
    * friendship_service.accept_friend_request() now simply creates a new instance of Friendship

--- api-core/src/friends/friendship_service.py
+++ api-core/src/friends/friendship_service.py
@@ -1,72 +1,7 @@
-import dataclasses
+import base
 import datetime
-import enum
 import friendship_repository
-import repositories
 import uuid
-
-def _generate_id(id1: uuid.UUID, id2: uuid.UUID) -> frozenset:
-    """Utility function to generate the key of a Friendship between two users.
-    
-    Args:
-        id1: the UUID of one friend.
-        id2: the UUID of the other friend.
-
-    Raises:
-        ValueError: if the two IDs are equal.
-    
-    See Also:
-        Friendship
-    """
-
-    if id1 == id2:
-        raise(ValueError, "id1 and id2 cannot be equal.")
-    
-    id_list = [id1, id2]
-    return frozenset(id_list)
-
-class FriendshipStatus(enum.Enum):
-    """
-    Enum class defining the two possible states of a friendship.
-     
-    A friendship can either be awaiting a review by the target friend (the user
-    pointed to by friend_id), or it can have been accepted.
-    """
-    PENDING = "pending"
-    ACCEPTED = "accepted"
-
-@dataclasses.dataclass(frozen = True)
-class Friendship(repositories.IEntity):
-    """
-    Defines the core structure for friendship service relationship in the 
-    api-core service.
-
-    Note that user_id represents the ID of the user who originally sent the
-    request; friend_id represents the user who recieves and must approve/decline
-    said request.
-
-    All attributes are read-only, except for friendship_status, which can be
-    updated from PENDING to ACCEPTED by calling the accept() method.
-
-    Args:
-        user_id: The ID of the user sending the request.
-        friend_id: The ID of the user recieving the request.
-        created_at: The datetime of when the friend request was sent.
-        friendship_status: Whether or not the friend request has been accepted
-            yet.
-    """
-    user_id: uuid.UUID
-    friend_id: uuid.UUID
-    created_at: datetime.datetime
-    friendship_status: FriendshipStatus
-
-    def get_id(self) -> frozenset:
-        return _generate_id(self.user_id, self.friend_id)
-    
-    def _accept(self) -> None:
-        """Updates the status of the friend request to ACCEPTED."""
-
-        self.friendship_status = FriendshipStatus.ACCEPTED
 
 repository = friendship_repository.InMemoryFriendshipRepository()
 
@@ -83,8 +18,8 @@ def sendFriendRequest(user_id: uuid.UUID, friend_id: uuid.UUID) -> None:
     """
 
     time = datetime.datetime.now()
-    status = FriendshipStatus.PENDING
-    friendship = Friendship(user_id, friend_id, time, status)
+    status = base.FriendshipStatus.PENDING
+    friendship = base.Friendship(user_id, friend_id, time, status)
 
     repository.save(friendship)
 
@@ -100,11 +35,16 @@ def acceptFriendRequest(id1: uuid.UUID, id2: uuid.UUID) -> None:
         FriendshipStatus
     """
 
-    friendship_id = _generate_id(id1, id2)
+    friendship_id = base._generate_id(id1, id2)
 
     friendship = repository.find_by_id(friendship_id)
-    friendship._accept()
-    repository.save(friendship_id)
+    # Create a new Friendship instance as they are immutable.
+    status = base.FriendshipStatus.ACCEPTED
+    friendship = base.Friendship(friendship.user_id,
+                                 friendship.friend_id,
+                                 friendship.created_at,
+                                 status)
+    repository.save(friendship)
 
 def removeFriend(id1: uuid.UUID, id2: uuid.UUID) -> None:
     """Removes the Frienship record between two users from the repository.
@@ -117,7 +57,7 @@ def removeFriend(id1: uuid.UUID, id2: uuid.UUID) -> None:
         Friendship
     """
 
-    friendship_id = _generate_id(id1, id2)
+    friendship_id = base._generate_id(id1, id2)
     repository.delete(friendship_id)
 
 def getFriends(user_id: uuid.UUID) -> list[uuid.UUID]:
@@ -170,7 +110,7 @@ def isFriend(id1: uuid.UUID, id2: uuid.UUID) -> bool:
         Whether the two users are friends.
     """
 
-    friendship_id = _generate_id(id1, id2)
+    friendship_id = base._generate_id(id1, id2)
     friendship = repository.find_by_id(friendship_id)
 
     # Predicate resolves to true if the friendship exists within the repository;
