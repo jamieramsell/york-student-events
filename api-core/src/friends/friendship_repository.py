4e7da8918396d4e76ad886f9137e249e38a38495
Jamie <jamieramsell07@outlook.com>
2026-06-20 02:45:07 +0100

    feat: implement friendship_repository, a module storing the InMemoryFriendshipRepository class.
    * InMemoryFriendshipRepository is a dictionary backed repository for storing and retrieving Friendship entities
    
    * extends repositories.IRepository with frozenset as the managed type, providing operations scoped to the frozenset keys of Friendship objects.
    
    * used for integration testing before implementing database-backed repositories.

---
+++ api-core/src/friends/friendship_repository.py
@@ -0,0 +1,32 @@
+import repositories
+import friendship_service
+
+class InMemoryFriendshipRepository(repositories.IRepository[frozenset]):
+    """Dictionary backed repository for storing and retrieving Friendship
+    entities.
+    
+    Extends repositories.IRepository with frozenset as the managed type,
+    providing standard CRUD operations scoped to the frozenset keys of
+    Friendship objects. Used for integration testing before implementing
+    database-backed repositories.
+    
+    See Also:
+        repositories.IRepository
+    """
+
+    def __init__(self):
+        self.__dict = {}
+
+    def save(self, entity: friendship_service.Friendship) -> None:
+        self.__dict[entity.getId()] = entity
+
+    def delete(self, entity_id: frozenset) -> None:
+        self.__dict.pop(entity_id)
+
+    def find_by_id(
+        self, entity_id: frozenset
+    ) -> friendship_service.Friendship | None:
+        return self.__dict.get(entity_id)
+
+    def find_all(self) -> list[friendship_service.Friendship]:
+        return self.__dict.values
