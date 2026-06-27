1fec02f26fa9b797fb79d3facb162eebb9348542
Jamie <jamieramsell07@outlook.com>
2026-06-27 20:32:49 +0100

    feat: define IObserver

--- event-service/src/main/java/york/studentevents/subscriptions/IObserver.java
+++ event-service/src/main/java/york/studentevents/subscriptions/IObserver.java
@@ -1,4 +1,20 @@
 package york.studentevents.subscriptions;
 
-/** todo. */
-public interface IObserver {}
+import york.studentevents.events.IEvent;
+
+/**
+ * Defines the contract for any class that wants to receive event notifications.
+ *
+ * <p>Forms part of the Observer pattern implementation.
+ */
+public interface IObserver {
+
+  /**
+   * Method, called from an IObservable, whenever information about the observable event is updated.
+   * 
+   * @param source The object being observed.
+   * @param event The updated state of the event in question. 
+   */
+  void update(IObservable source, IEvent event);
+
+}
