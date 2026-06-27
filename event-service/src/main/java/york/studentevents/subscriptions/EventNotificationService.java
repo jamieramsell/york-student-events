b8d667e5fa6c99cbe207bced387635242e98b47a
Jamie <jamieramsell07@outlook.com>
2026-06-27 20:51:20 +0100

    feat: define EventNotificationService as a class which implements IObservable

--- event-service/src/main/java/york/studentevents/subscriptions/EventNotificationService.java
+++ event-service/src/main/java/york/studentevents/subscriptions/EventNotificationService.java
@@ -1,4 +1,35 @@
 package york.studentevents.subscriptions;
 
+import java.util.HashSet;
+import java.util.Set;
+import york.studentevents.events.IEvent;
+
 /** todo. */
-public class EventNotificationService {}
+public class EventNotificationService implements IObservable {
+
+  private static final double CAPACITY_WARNING_THRESHOLD = 0.8;
+  private final Set<IObserver> observers;
+
+  /** todo. */
+  public EventNotificationService() {
+    observers = new HashSet<>();
+  }
+
+  @Override
+  public boolean attach(IObserver observer) {
+    return observers.add(observer);
+  }
+
+  @Override
+  public boolean detach(IObserver observer) {
+    return observers.remove(observer);
+  }
+
+  @Override
+  public void notifyObservers(IEvent event) {
+    for (IObserver observer : observers) {
+      observer.update(this, event);
+    }
+  }
+
+}
