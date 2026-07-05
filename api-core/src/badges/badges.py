88d567d391ca61dd81f3d1586375254f3cccfdc5
Jamie <jamieramsell07@outlook.com>
2026-07-05 14:01:07 +0100

    refactor: make Badge inherit from repositories.IEntity

--- api-core/src/badges/badges.py
+++ api-core/src/badges/badges.py
@@ -1,12 +1,29 @@
-from dataclasses import dataclass
+import dataclasses
+import collections.abc
+import repositories
+import typing
+import uuid
 
-@dataclass
-class Badge:
-    """
-    Defines the core data structures for a badge in the Python api-core service
-    """
+# Define Predicate type alias, representing a callable, taking kwargs input,
+# and returning a boolean value
+type Predicate = collections.abc.Callable[[dict[typing.Any, typing.Any]], bool]
 
-    id: int
-    award_condition: str #to be made more structure at M4
+@dataclasses.dataclass(frozen = True)
+class Badge(repositories.IEntity[uuid.UUID]):
+    """
+    Defines the core structure of a Badge in the ``api-core`` service.
+
+    Args:
+        id: The ID of the badge; must not be None.
+        name: The display name of the Badge; must not be None.
+        description: An optional extra description of the Badge.
+        award_condition: A predicate which represents the condition(s) which
+            must be fulfilled in order to award the badge to a user
+    """
+    id: uuid.UUID
     name: str
-    description: str
+    description: str | None
+    award_condition: Predicate
+
+    def get_id(self) -> uuid.UUID:
+        return self.id
