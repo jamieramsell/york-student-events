1f80c64af54a9a62776464e31439a4a37846b1b7
Jamie <jamieramsell07@outlook.com>
2026-07-24 13:44:34 +0100

    feat: define SQLAlchemyAttendanceRepository

--- api-core/src/attendance/attendance_repository.py
+++ api-core/src/attendance/attendance_repository.py
@@ -6,9 +6,12 @@ the IDs of Attendance records. This stands in for a database-backed repository
 during early development.
 """
 import datetime
+import sqlalchemy
+import typing
 import uuid
 
 import repositories
+import repositories.sql
 from attendance import base
 
 
@@ -58,3 +61,47 @@ class InMemoryCannedAttendanceRepository(InMemoryAttendanceRepository):
                 datetime.datetime.now(datetime.timezone.utc),
             )
         )
+
+
+class SQLAlchemyAttendanceRepository(
+    repositories.sql.SqlAlchemyRepository[base.AttendanceId, base.Attendance]
+):
+    """Database backed repository for storing and retrieving Attendance
+    records.
+    
+    Extends repositories.sql.SqlAlchemyRepository with
+    ``tuple[uuid.UUID, uuid.UUID]`` as the managed type, providing standard CRUD
+    operations scoped to the keys (formed of a 2-tuple of UUID keys) of
+    Attendance records. 
+
+    See Also:
+        repositories.IRepository
+        repositories.sql.SqlAlchemyRepository
+    """
+    @property
+    def _table(self) -> sqlalchemy.Table:
+        return repositories.sql.attendance
+    
+
+    def _id_predicate(
+        self, entity_id: base.AttendanceId
+    ) -> sqlalchemy.ColumnElement[bool]:
+        return sqlalchemy.and_(self._table.c.attendee_id == entity_id[0],
+                               self._table.c.event_id == entity_id[1])
+
+
+    def _to_row(self, entity: base.Attendance) -> dict[str, typing.Any]:
+        return {
+            "attendee_id": entity.attendee_id,
+            "event_id": entity.event_id,
+            "recorded_at": entity.recorded_at
+        }
+    
+
+    def _from_row(self, row: sqlalchemy.Row[typing.Any]) -> base.Attendance:
+        attendee_id = row.attendee_id
+        event_id = row.event_id
+        recorded_at = row.recorded_at
+
+        attendance_record = base.Attendance(attendee_id, event_id, recorded_at)
+        return attendance_record
