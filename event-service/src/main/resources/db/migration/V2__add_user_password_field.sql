7231faa7cef74412f112e57f1f377af3f1a83616
Jamie <jamieramsell07@outlook.com>
2026-08-05 14:22:03 +0100

    fix: split migration into two spereate statements, so that it is supported by H2 as well as postgres

--- event-service/src/main/resources/db/migration/V2__add_user_password_field.sql
+++ event-service/src/main/resources/db/migration/V2__add_user_password_field.sql
@@ -1,4 +1,2 @@
-ALTER TABLE users (
-    DROP COLUMN cohort,
-    ADD password_hash varchar NOT NULL
-);
+ALTER TABLE users DROP COLUMN cohort;
+ALTER TABLE users ADD COLUMN password_hash varchar NOT NULL;
